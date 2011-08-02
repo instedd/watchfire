4086d2c14867ca281fe76aea55474f7df80fecfd
Adrian Romero <aromero@manas.com.ar>
2011-08-02 11:08:57 -0300

    Initial rails scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watchfire::Application.config.secret_token = '7e9ef94c2e0cad6efd3bd230904ed882b3c49da334d3ff9767053f127bd062d9dabbb93bb4f43fd225ac54b6db8f0fd01a0a1c096d0a345f67cdb2ccb1a5a9cf'
