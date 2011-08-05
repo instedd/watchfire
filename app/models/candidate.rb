8eb93a65e64afbf5c72e20976878fd91095c0395
Andres Taraciuk <ataraciuk@manas.com.ar>
2011-08-05 13:53:43 -0300

    defaults values for candidate model

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -8,16 +8,23 @@ class Candidate < ActiveRecord::Base
   validates_presence_of :mission_id, :volunteer_id, :voice_retries, :sms_retries
 
   validates_numericality_of :voice_retries, :only_integer => true, :greater_than_or_equal_to => 0
-  validates_numericality_of :voice_retries, :only_integer => true, :greater_than_or_equal_to => 0
+  validates_numericality_of :sms_retries, :only_integer => true, :greater_than_or_equal_to => 0
 
   validate :not_same_volunteer_same_mission
+
+	after_initialize :init
 
   private
 
   def not_same_volunteer_same_mission
     if Candidate.where("mission_id = ? AND volunteer_id = ?", self.mission_id, self.volunteer_id).count > 0
-      errors[:base] << 'The volunteer is already in the candidate list'
+      errors[:base] << 'The volunteer is already in the candidate list for the mission'
     end
   end
+
+	def init
+		self.voice_retries ||= 0
+		self.sms_retries ||= 0
+	end
 
 end
