76bd3b2d6f32fcc3f450cc1be566272d097f1182
Adrian Romero <aromero@manas.com.ar>
2011-08-06 15:05:01 -0300

    Initial version of the nuntium controller to receive ATs

--- app/controllers/nuntium_controller.rb
+++ app/controllers/nuntium_controller.rb
@@ -1,7 +1,27 @@
 class NuntiumController < ApplicationController
   
   def receive
-    puts params[:body]
+    from = params[:from]
+    body = params[:body]
+    
+    number_match = from.match /sms:\/\/(\d+)/
+    response_match = body.match /(1|2)/
+    
+    return unless number_match || response_match
+    
+    number = number_match[1]
+    response = response_match[1].to_i
+    
+    candidate = Candidate.joins(:volunteer).where(:volunteers => {:sms_number => number}).order('last_sms_att DESC').first
+    
+    if response == 1
+      candidate.status = :confirmed
+    elsif response == 2
+      candidate.status = :denied
+    end
+    
+    candidate.save!
+    
     render :nothing => true
   end
 
