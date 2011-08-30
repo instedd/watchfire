954b111525e3b136a03e5b72d5c442b6cb5367c6
Andres Taraciuk <ataraciuk@manas.com.ar>
2011-08-30 17:41:22 -0300

    scope for active skills

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,7 @@
 class Skill < ActiveRecord::Base
 
+	scope :actives, where('id IN (SELECT skill_id from skills_volunteers) OR id IN (SELECT skill_id from missions)')
+
 	validates_presence_of :name
 
 	has_and_belongs_to_many :volunteers
