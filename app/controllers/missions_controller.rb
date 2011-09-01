734d07f0873b11236e65d9668201037bc3e0df95
Andres Taraciuk <ataraciuk@manas.com.ar>
2011-09-01 17:26:41 -0300

    added mission list

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,11 +1,20 @@
 class MissionsController < ApplicationController
 
 	before_filter :authenticate_user!
+	before_filter :check_owner, :except => [:create, :new, :index]
 
-  def index
-    @mission = Mission.first || Mission.new
+  def show
 		@distance = @mission.obtain_farthest
   end
+
+	def new
+		@mission = Mission.new
+		render 'show'
+	end
+
+	def index
+		@missions = Mission.where(:user_id => current_user.id).order('id DESC')
+	end
 
 	def create
 		@mission = Mission.new(params[:mission])
@@ -16,7 +25,6 @@ class MissionsController < ApplicationController
 	end
 
 	def update
-		@mission = Mission.find(params[:id])
 		@mission.attributes = params[:mission]
 		if @mission.need_check_candidates
 			@mission.check_and_save
@@ -27,17 +35,14 @@ class MissionsController < ApplicationController
 	end
 	
 	def start
-	  @mission = Mission.find(params[:id])
 	  @mission.call_volunteers
 	end
 	
 	def stop
-	  @mission = Mission.find(params[:id])
 	  @mission.stop_calling_volunteers
   end
   
   def refresh
-    @mission = Mission.find(params[:id])
 		@distance = @mission.obtain_farthest
     respond_to do |format|
       format.html { render 'index' }
@@ -46,9 +51,15 @@ class MissionsController < ApplicationController
   end
 
 	def destroy
-		@mission = Mission.find(params[:id])
 		@mission.destroy
 		redirect_to root_path
+	end
+
+	private
+
+	def check_owner
+		@mission = Mission.find(params[:id])
+		redirect_to missions_path unless @mission.user == current_user
 	end
 
 end
