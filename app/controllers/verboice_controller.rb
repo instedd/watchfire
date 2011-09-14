61c091d1717bf883c2ad7ae6ded86423d158a638
Adrian Romero <aromero@manas.com.ar>
2011-09-14 17:50:20 -0300

    Fixed verboice callback controller when digits don't match 1 or 2

--- app/controllers/verboice_controller.rb
+++ app/controllers/verboice_controller.rb
@@ -5,6 +5,10 @@ class VerboiceController < ApplicationController
   end
   
   def callback
+    # find candidate based on call id
+    @candidate = Candidate.find_by_call_id params[:CallSid]
+    
+    # look for user response based on digits
     match = params[:Digits].match /(1|2)/
     
     # digits don't match required response, play 'plan' again
@@ -13,16 +17,13 @@ class VerboiceController < ApplicationController
       return
     end
     
-    # find candidate based on call id
-    candidate = Candidate.find_by_call_id params[:CallSid]
-    
     # update status according to response
     answer = match[1].to_i
     
     if answer == 1
-      candidate.update_status :confirmed
+      @candidate.update_status :confirmed
     else
-      candidate.update_status :denied
+      @candidate.update_status :denied
     end
   end
 
