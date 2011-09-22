5887ef49e04a7f59a2b3632083503b93ada2f953
Adrian Romero <aromero@manas.com.ar>
2011-09-22 16:44:57 -0300

    Added style to column sorting in volunteers idnex

--- app/helpers/volunteers_helper.rb
+++ app/helpers/volunteers_helper.rb
@@ -1,7 +1,24 @@
 module VolunteersHelper
   
-  def sort_link name, order
-    link_to name, :action => :index, :order => order, :page => @page, :direction => @order == order.to_s ? 'DESC' : 'ASC'
+  def sort_header name, order
+    selected = @order == order.to_s
+    dir_class = @direction == 'DESC' ? 'down' : 'up'
+    url = url_for :action => :index, :order => order, :page => @page, :direction => selected ? invert(@direction) : 'ASC'
+    content_tag :th, :class => "link sort #{dir_class if selected}", 'data-url' => url do
+      concat name
+      concat content_tag :span
+    end
+  end
+  
+  private
+  
+  def invert order
+    case order
+    when 'DESC'
+      'ASC'
+    else
+      'DESC'
+    end
   end
   
 end
