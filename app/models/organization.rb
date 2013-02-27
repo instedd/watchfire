586126cb2f18bfda5c323bb35a2700d904b04fd8
Gustavo Giráldez <ggiraldez@manas.com.ar>
2013-02-27 11:52:29 -0300

    Fix #38: Negative values shouldn't be allowed when creating an organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -7,4 +7,9 @@ class Organization < ActiveRecord::Base
   has_many :invites
 
   validates_presence_of :name
+
+  validates_numericality_of :max_sms_retries, :only_integer => true, :greater_than_or_equal_to => 1
+  validates_numericality_of :max_voice_retries, :only_integer => true, :greater_than_or_equal_to => 1
+  validates_numericality_of :sms_timeout, :only_integer => true, :greater_than_or_equal_to => 0
+  validates_numericality_of :voice_timeout, :only_integer => true, :greater_than_or_equal_to => 0
 end
