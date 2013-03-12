75e4e87378327bbb29225c03e8004852bfa2f84f
Gustavo Giráldez <ggiraldez@manas.com.ar>
2013-03-12 18:38:13 -0300

    Fix #66: Number validation on New organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -9,8 +9,8 @@ class Organization < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name, :case_sensitive => false
 
-  validates_numericality_of :max_sms_retries, :only_integer => true, :greater_than_or_equal_to => 1
-  validates_numericality_of :max_voice_retries, :only_integer => true, :greater_than_or_equal_to => 1
-  validates_numericality_of :sms_timeout, :only_integer => true, :greater_than_or_equal_to => 0
-  validates_numericality_of :voice_timeout, :only_integer => true, :greater_than_or_equal_to => 0
+  validates_numericality_of :max_sms_retries, :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 20
+  validates_numericality_of :max_voice_retries, :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 20
+  validates_numericality_of :sms_timeout, :only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 120
+  validates_numericality_of :voice_timeout, :only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 120
 end
