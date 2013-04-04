e34f057030a3035b94e56986fb396c42897a0560
Adrian Romero <aromero@manas.com.ar>
2013-04-04 14:00:50 -0300

    Fixed Issue #105 Reconfirm email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -212,4 +212,7 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
   # end
+
+  # Reconfirmable
+  config.reconfirmable = true
 end
