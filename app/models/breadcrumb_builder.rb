4220311b3e032aff5e80fcffb58ee1f8680849c1
Adrian Romero <aromero@manas.com.ar>
2013-04-04 14:22:02 -0300

    Fixed Issue #97 Escape html in breadcrumb

--- app/models/breadcrumb_builder.rb
+++ app/models/breadcrumb_builder.rb
@@ -1,11 +1,11 @@
 class BreadcrumbBuilder < BreadcrumbsOnRails::Breadcrumbs::Builder
-  
+
   def render
     "<ul>#{@elements.map{|e| "<li>#{item e}</li>"}.join}</ul>"
   end
-  
+
   def item element
-    @context.link_to_unless_current(compute_name(element), compute_path(element))
+    @context.link_to_unless_current(@context.html_escape(compute_name(element)), compute_path(element))
   end
-  
+
 end
