28fabff7b707125bf1105de0fbefb1b30f81f314
Mauri <mmuller@manas.com.ar>
2013-04-29 12:12:36 -0300

    Added Capybara helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,16 +3,48 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'mocha/setup'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/integration/spec/helpers/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   # == Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
   #
+
+  ###########
+  #capybara
+  config.include Warden::Test::Helpers
+  config.include Capybara::DSL,           example_group: { file_path: config.escaped_path(%w[spec integration])}
+  config.include Capybara::CustomFinders, example_group: { file_path: config.escaped_path(%w[spec integration])}
+  config.include Capybara::AccountHelper, example_group: { file_path: config.escaped_path(%w[spec integration])}
+  config.include Capybara::EventHelper, example_group: { file_path: config.escaped_path(%w[spec integration])}
+  config.filter_run_excluding(js: true)   unless config.filter_manager.inclusions[:js]
+
+  Warden.test_mode!
+  
+  Capybara.default_wait_time = 5
+  Capybara.javascript_driver = :selenium
+  Capybara.default_selector = :css
+
+  config.before :each do
+    DatabaseCleaner.strategy = if Capybara.current_driver == :rack_test
+      :transaction
+    else
+      [:truncation]
+    end
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
+  ##########
+
   config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
